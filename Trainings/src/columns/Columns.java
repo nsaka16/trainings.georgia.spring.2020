1d5bee9c8225bcd2194ac93e9756294df6857720
nikasakana@gmail.com <nikasakana@gmail.com>
2020-05-07 21:17:55 +0400

    Mysterious names-code smell of columns game solved

--- Trainings/src/columns/Columns.java
+++ Trainings/src/columns/Columns.java
@@ -6,41 +6,38 @@ import java.util.*;
 
 public class Columns extends Applet implements Runnable {
     static final int
-    SL=25,
-    Depth=15,
-    Width=7,
-    MaxLevel=7,
+    SCALE_OF_GAME_COMPONENTS=25,
+    FIELD_DEPTH=15,
+    FIELD_WIDTH=7,
+    FIELD_LEFT_OFFSET=2,
+    FIELD_TOP_OFFSET=2,
+    MAX_LEVEL=7,
     TimeShift=250,
     FigToDrop=33,
-    MinTimeShift=200,
-    LeftBorder=2,
-    TopBorder=2;
-    
-    
-    Color MyStyles[] = {Color.black,Color.cyan,Color.blue,Color.red,Color.green,
+    MinTimeShift=200;
+    	
+    private Color colors[] = {Color.black,Color.cyan,Color.blue,Color.red,Color.green,
     Color.yellow,Color.pink,Color.magenta,Color.black};
     
-    int Level, i, j, ii, k, ch;
-    long Score, DScore, tc;
-    Font fCourier;
-    Figure Fig;
-    int Fnew[][],Fold[][];
+    int currentLevel, k, playerKeyPressed;
+    long playerScore, playerScoreAddition;
+    Figure figure;
+    int newField[][],oldField[][];
     boolean NoChanges = true, KeyPressed = false;
-    Graphics _gr;
+    Graphics graphics;
     
-    Thread thr = null;
-    
+    Thread thread = null;
     
     void CheckNeighbours(int a, int b, int c, int d, int i, int j) {
-        if ((Fnew[j][i]==Fnew[a][b]) && (Fnew[j][i]==Fnew[c][d])) {
-            Fold[a][b] = 0;
+        if ((newField[j][i]==newField[a][b]) && (newField[j][i]==newField[c][d])) {
+            oldField[a][b] = 0;
             DrawBox(a,b,8);
-            Fold[j][i] = 0;
+            oldField[j][i] = 0;
             DrawBox(j,i,8);
-            Fold[c][d] = 0;
+            oldField[c][d] = 0;
             DrawBox(c,d,8);
             NoChanges = false;
-            Score += (Level+1)*10;
+            playerScore += (currentLevel+1)*10;
             k++;
         };
     }
@@ -50,88 +47,86 @@ public class Columns extends Applet implements Runnable {
         }
         catch (InterruptedException e) {};
     }
-    void DrawBox(int x, int y, int c) {
-        if (c==0) {
-            _gr.setColor(Color.black);
-            _gr.fillRect(LeftBorder+x*SL-SL, TopBorder+y*SL-SL, SL, SL);
-            _gr.drawRect(LeftBorder+x*SL-SL, TopBorder+y*SL-SL, SL, SL);
+    void DrawBox(int col, int row, int color) {
+        if (color==0) {
+            graphics.setColor(Color.black);
+            graphics.fillRect(FIELD_LEFT_OFFSET+col*SCALE_OF_GAME_COMPONENTS-SCALE_OF_GAME_COMPONENTS, FIELD_TOP_OFFSET+row*SCALE_OF_GAME_COMPONENTS-SCALE_OF_GAME_COMPONENTS, SCALE_OF_GAME_COMPONENTS, SCALE_OF_GAME_COMPONENTS);
+            graphics.drawRect(FIELD_LEFT_OFFSET+col*SCALE_OF_GAME_COMPONENTS-SCALE_OF_GAME_COMPONENTS, FIELD_TOP_OFFSET+row*SCALE_OF_GAME_COMPONENTS-SCALE_OF_GAME_COMPONENTS, SCALE_OF_GAME_COMPONENTS, SCALE_OF_GAME_COMPONENTS);
         }
-        else if (c==8) {
-            _gr.setColor(Color.white);
-            _gr.drawRect(LeftBorder+x*SL-SL+1, TopBorder+y*SL-SL+1, SL-2, SL-2);
-            _gr.drawRect(LeftBorder+x*SL-SL+2, TopBorder+y*SL-SL+2, SL-4, SL-4);
-            _gr.setColor(Color.black);
-            _gr.fillRect(LeftBorder+x*SL-SL+3, TopBorder+y*SL-SL+3, SL-6, SL-6);
+        else if (color==8) {
+            graphics.setColor(Color.white);
+            graphics.drawRect(FIELD_LEFT_OFFSET+col*SCALE_OF_GAME_COMPONENTS-SCALE_OF_GAME_COMPONENTS+1, FIELD_TOP_OFFSET+row*SCALE_OF_GAME_COMPONENTS-SCALE_OF_GAME_COMPONENTS+1, SCALE_OF_GAME_COMPONENTS-2, SCALE_OF_GAME_COMPONENTS-2);
+            graphics.drawRect(FIELD_LEFT_OFFSET+col*SCALE_OF_GAME_COMPONENTS-SCALE_OF_GAME_COMPONENTS+2, FIELD_TOP_OFFSET+row*SCALE_OF_GAME_COMPONENTS-SCALE_OF_GAME_COMPONENTS+2, SCALE_OF_GAME_COMPONENTS-4, SCALE_OF_GAME_COMPONENTS-4);
+            graphics.setColor(Color.black);
+            graphics.fillRect(FIELD_LEFT_OFFSET+col*SCALE_OF_GAME_COMPONENTS-SCALE_OF_GAME_COMPONENTS+3, FIELD_TOP_OFFSET+row*SCALE_OF_GAME_COMPONENTS-SCALE_OF_GAME_COMPONENTS+3, SCALE_OF_GAME_COMPONENTS-6, SCALE_OF_GAME_COMPONENTS-6);
         }
         else {
-            _gr.setColor(MyStyles[c]);
-            _gr.fillRect(LeftBorder+x*SL-SL, TopBorder+y*SL-SL, SL, SL);
-            _gr.setColor(Color.black);
-            _gr.drawRect(LeftBorder+x*SL-SL, TopBorder+y*SL-SL, SL, SL);
+            graphics.setColor(colors[color]);
+            graphics.fillRect(FIELD_LEFT_OFFSET+col*SCALE_OF_GAME_COMPONENTS-SCALE_OF_GAME_COMPONENTS, FIELD_TOP_OFFSET+row*SCALE_OF_GAME_COMPONENTS-SCALE_OF_GAME_COMPONENTS, SCALE_OF_GAME_COMPONENTS, SCALE_OF_GAME_COMPONENTS);
+            graphics.setColor(Color.black);
+            graphics.drawRect(FIELD_LEFT_OFFSET+col*SCALE_OF_GAME_COMPONENTS-SCALE_OF_GAME_COMPONENTS, FIELD_TOP_OFFSET+row*SCALE_OF_GAME_COMPONENTS-SCALE_OF_GAME_COMPONENTS, SCALE_OF_GAME_COMPONENTS, SCALE_OF_GAME_COMPONENTS);
         }
         //		g.setColor (Color.black);
     }
     void DrawField(Graphics g) {
-        int i,j;
-        for (i=1; i<=Depth; i++) {
-            for (j=1; j<=Width; j++) {
-                DrawBox(j,i,Fnew[j][i]);
+        for (int row=1; row<=FIELD_DEPTH; row++) {
+            for (int col=1; col<=FIELD_WIDTH; col++) {
+                DrawBox(col,row,newField[col][row]);
             }
         }
     }
     void DrawFigure(Figure f) {
-        DrawBox(f.x,f.y,f.c[1]);
-        DrawBox(f.x,f.y+1,f.c[2]);
-        DrawBox(f.x,f.y+2,f.c[3]);
+        DrawBox(f.column,f.row,f.colorsOfFigureBoxes[1]);
+        DrawBox(f.column,f.row+1,f.colorsOfFigureBoxes[2]);
+        DrawBox(f.column,f.row+2,f.colorsOfFigureBoxes[3]);
     }
     void DropFigure(Figure f) {
         int zz;
-        if (f.y < Depth-2) {
-            zz = Depth;
-            while (Fnew[f.x][zz]>0) zz--;
-            DScore = (((Level+1)*(Depth*2-f.y-zz) * 2) % 5) * 5;
-            f.y = zz-2;
+        if (f.row < FIELD_DEPTH-2) {
+            zz = FIELD_DEPTH;
+            while (newField[f.column][zz]>0) zz--;
+            playerScoreAddition = (((currentLevel+1)*(FIELD_DEPTH*2-f.row-zz) * 2) % 5) * 5;
+            f.row = zz-2;
         }
     }
-    boolean FullField() {
-        int i;
-        for (i=1; i<=Width; i++) {
-            if (Fnew[i][3]>0)
+    
+    boolean isFieldFull() {
+        for (int col=1; col<=FIELD_WIDTH; col++) {
+            if (newField[col][3]>0)
                 return true;
         }
         return false;
     }
-    void HideFigure(Figure f) {
-        DrawBox(f.x,f.y,0);
-        DrawBox(f.x,f.y+1,0);
-        DrawBox(f.x,f.y+2,0);
+    void HideFigure(Figure figure) {
+        DrawBox(figure.column,figure.row,0);
+        DrawBox(figure.column,figure.row+1,0);
+        DrawBox(figure.column,figure.row+2,0);
     }
     public void init() {
-        Fnew = new int[Width+2][Depth+2];
-        Fold = new int[Width+2][Depth+2];
-        _gr = getGraphics();
+        newField = new int[FIELD_WIDTH+2][FIELD_DEPTH+2];
+        oldField = new int[FIELD_WIDTH+2][FIELD_DEPTH+2];
+        graphics = getGraphics();
     }
     public boolean keyDown(Event e, int k) {
         KeyPressed = true;
-        ch = e.key;
+        playerKeyPressed = e.key;
         return true;
     }
     public boolean lostFocus(Event e, Object w) {
         KeyPressed = true;
-        ch = 'P';
+        playerKeyPressed = 'P';
         return true;
     }
     void PackField() {
-        int i,j,n;
-        for (i=1; i<=Width; i++) {
-            n = Depth;
-            for (j=Depth; j>0; j--) {
-                if (Fold[i][j]>0) {
-                    Fnew[i][n] = Fold[i][j];
+        for (int col=1; col<=FIELD_WIDTH; col++) {
+            int n = FIELD_DEPTH;
+            for (int row=FIELD_DEPTH; row>0; row--) {
+                if (oldField[col][row]>0) {
+                    newField[col][row] = oldField[col][row];
                     n--;
                 }
             };
-            for (j=n; j>0; j--) Fnew[i][j] = 0;
+            for (int j=n; j>0; j--) newField[col][j] = 0;
         }
     }
     public void paint(Graphics g) {
@@ -142,175 +137,173 @@ public class Columns extends Applet implements Runnable {
         ShowLevel(g);
         ShowScore(g);
         DrawField(g);
-        DrawFigure(Fig);
+        DrawFigure(figure);
         requestFocus();
     }
-    void PasteFigure(Figure f) {
-        Fnew[f.x][f.y] = f.c[1];
-        Fnew[f.x][f.y+1] = f.c[2];
-        Fnew[f.x][f.y+2] = f.c[3];
+    void PasteFigure(Figure figure) {
+    	for(int i=0; i<3; i++) {
+    		newField[figure.column][figure.row + i] = figure.colorsOfFigureBoxes[i+1];
+    	}
     }
     public void run() {
-        for (i=0; i<Width+1; i++){
-            for (j=0; j<Depth+1; j++) {
-                Fnew[i][j] = 0;
-                Fold[i][j] = 0;
+        for (int col=0; col<FIELD_WIDTH+1; col++){
+            for (int row=0; row<FIELD_DEPTH+1; row++) {
+                newField[col][row] = 0;
+                oldField[col][row] = 0;
             }
         }
-        Level = 0;
-        Score = 0;
-        j = 0;
+        currentLevel = 0;
+        playerScore = 0;
+        int i = 0;
         k = 0;
-        _gr.setColor(Color.black);
+        graphics.setColor(Color.black);
         requestFocus();
-        
         do {
-            tc = System.currentTimeMillis();
+            long tc = System.currentTimeMillis();
             new Figure();
-            DrawFigure(Fig);
-            while ((Fig.y<Depth-2) && (Fnew[Fig.x][Fig.y+3]==0)) {
-                if ((int)(System.currentTimeMillis()-tc)>(MaxLevel-Level)*TimeShift+MinTimeShift) {
+            DrawFigure(figure);
+            while ((figure.row<FIELD_DEPTH-2) && (newField[figure.column][figure.row+3]==0)) {
+                if ((int)(System.currentTimeMillis()-tc)>(MAX_LEVEL-currentLevel)*TimeShift+MinTimeShift) {
                     tc = System.currentTimeMillis();
-                    HideFigure(Fig);
-                    Fig.y++;
-                    DrawFigure(Fig);
+                    HideFigure(figure);
+                    figure.row++;
+                    DrawFigure(figure);
                 }
-                DScore = 0;
+                playerScoreAddition = 0;
                 do {
                     Delay(50);
                     if (KeyPressed) {
                         KeyPressed = false;
-                        switch (ch) {
+                        switch (playerKeyPressed) {
                             case Event.LEFT:
-                                if ((Fig.x>1) && (Fnew[Fig.x-1][Fig.y+2]==0)) {
-                                    HideFigure(Fig);
-                                    Fig.x--;
-                                    DrawFigure(Fig);
+                                if ((figure.column>1) && (newField[figure.column-1][figure.row+2]==0)) {
+                                    HideFigure(figure);
+                                    figure.column--;
+                                    DrawFigure(figure);
                                 }
                                 break;
                             case Event.RIGHT:
-                                if ((Fig.x<Width) && (Fnew[Fig.x+1][Fig.y+2]==0)) {
-                                    HideFigure(Fig);
-                                    Fig.x++;
-                                    DrawFigure(Fig);
+                                if ((figure.column<FIELD_WIDTH) && (newField[figure.column+1][figure.row+2]==0)) {
+                                    HideFigure(figure);
+                                    figure.column++;
+                                    DrawFigure(figure);
                                 }
                                 break;
                             case Event.UP:
-                                i = Fig.c[1];
-                                Fig.c[1] = Fig.c[2];
-                                Fig.c[2] = Fig.c[3];
-                                Fig.c[3] = i;
-                                DrawFigure(Fig);
+                                i = figure.colorsOfFigureBoxes[1];
+                                figure.colorsOfFigureBoxes[1] = figure.colorsOfFigureBoxes[2];
+                                figure.colorsOfFigureBoxes[2] = figure.colorsOfFigureBoxes[3];
+                                figure.colorsOfFigureBoxes[3] = i;
+                                DrawFigure(figure);
                                 break;
                             case Event.DOWN:
-                                i = Fig.c[1];
-                                Fig.c[1] = Fig.c[3];
-                                Fig.c[3] = Fig.c[2];
-                                Fig.c[2] = i;
-                                DrawFigure(Fig);
+                                i = figure.colorsOfFigureBoxes[1];
+                                figure.colorsOfFigureBoxes[1] = figure.colorsOfFigureBoxes[3];
+                                figure.colorsOfFigureBoxes[3] = figure.colorsOfFigureBoxes[2];
+                                figure.colorsOfFigureBoxes[2] = i;
+                                DrawFigure(figure);
                                 break;
                             case ' ':
-                                HideFigure(Fig);
-                                DropFigure(Fig);
-                                DrawFigure(Fig);
+                                HideFigure(figure);
+                                DropFigure(figure);
+                                DrawFigure(figure);
                                 tc = 0;
                                 break;
                             case 'P':
                             case 'p':
                                 while (!KeyPressed) {
-                                    HideFigure(Fig);
+                                    HideFigure(figure);
                                     Delay(500);
-                                    DrawFigure(Fig);
+                                    DrawFigure(figure);
                                     Delay(500);
                                 }
                                 tc = System.currentTimeMillis();
                                 break;
                             case '-':
-                                if (Level > 0) Level--;
+                                if (currentLevel > 0) currentLevel--;
                                 k=0;
-                                ShowLevel(_gr);
+                                ShowLevel(graphics);
                                 break;
                             case '+':
-                                if (Level < MaxLevel) Level++;
+                                if (currentLevel < MAX_LEVEL) currentLevel++;
                                 k=0;
-                                ShowLevel(_gr);
+                                ShowLevel(graphics);
                                 break;
                         }
                     }
-                } while ( (int)(System.currentTimeMillis()-tc) <= (MaxLevel-Level)*TimeShift+MinTimeShift );
+                } while ( (int)(System.currentTimeMillis()-tc) <= (MAX_LEVEL-currentLevel)*TimeShift+MinTimeShift );
             };
-            PasteFigure(Fig);
+            PasteFigure(figure);
             do {
                 NoChanges = true;
                 TestField();
                 if (!NoChanges) {
                     Delay(500);
                     PackField();
-                    DrawField(_gr);
-                    Score += DScore;
-                    ShowScore(_gr);
+                    DrawField(graphics);
+                    playerScore += playerScoreAddition;
+                    ShowScore(graphics);
                     if (k>=FigToDrop) {
                         k = 0;
-                        if (Level<MaxLevel) Level++;
-                        ShowLevel(_gr);
+                        if (currentLevel<MAX_LEVEL) currentLevel++;
+                        ShowLevel(graphics);
                     }
                 }
             } while (!NoChanges);
-        }while (!FullField());
+        }while (!isFieldFull());
     }
     void ShowHelp(Graphics g) {
         g.setColor(Color.black);
         
-        g.drawString(" Keys available:",200-LeftBorder,102);
-        g.drawString("Roll Box Up:     ",200-LeftBorder,118);
-        g.drawString("Roll Box Down:   ",200-LeftBorder,128);
-        g.drawString("Figure Left:     ",200-LeftBorder,138);
-        g.drawString("Figure Right:    ",200-LeftBorder,148);
-        g.drawString("Level High/Low: +/-",200-LeftBorder,158);
-        g.drawString("Drop Figure:   space",200-LeftBorder,168);
-        g.drawString("Pause:           P",200-LeftBorder,180);
-        g.drawString("Quit:     Esc or Q",200-LeftBorder,190);
+        g.drawString(" Keys available:",200-FIELD_LEFT_OFFSET,102);
+        g.drawString("Roll Box Up:     ",200-FIELD_LEFT_OFFSET,118);
+        g.drawString("Roll Box Down:   ",200-FIELD_LEFT_OFFSET,128);
+        g.drawString("Figure Left:     ",200-FIELD_LEFT_OFFSET,138);
+        g.drawString("Figure Right:    ",200-FIELD_LEFT_OFFSET,148);
+        g.drawString("Level High/Low: +/-",200-FIELD_LEFT_OFFSET,158);
+        g.drawString("Drop Figure:   space",200-FIELD_LEFT_OFFSET,168);
+        g.drawString("Pause:           P",200-FIELD_LEFT_OFFSET,180);
+        g.drawString("Quit:     Esc or Q",200-FIELD_LEFT_OFFSET,190);
     }
     void ShowLevel(Graphics g) {
         g.setColor(Color.black);
-        g.clearRect(LeftBorder+100,390,100,20);
-        g.drawString("Level: "+Level,LeftBorder+100,400);
+        g.clearRect(FIELD_LEFT_OFFSET+100,390,100,20);
+        g.drawString("Level: "+currentLevel,FIELD_LEFT_OFFSET+100,400);
     }
     void ShowScore(Graphics g) {
         g.setColor(Color.black);
-        g.clearRect(LeftBorder,390,100,20);
-        g.drawString("Score: "+Score,LeftBorder,400);
+        g.clearRect(FIELD_LEFT_OFFSET,390,100,20);
+        g.drawString("Score: "+playerScore,FIELD_LEFT_OFFSET,400);
     }
     public void start() {
-        _gr.setColor(Color.black);
+        graphics.setColor(Color.black);
         
         //		setBackground (new Color(180,180,180));
         
-        if (thr == null) {
-            thr = new Thread(this);
-            thr.start();
+        if (thread == null) {
+            thread = new Thread(this);
+            thread.start();
         }
     }
     public void stop() {
-        if (thr != null) {
-            thr.stop();
-            thr = null;
+        if (thread != null) {
+            thread.stop();
+            thread = null;
         }
     }
     void TestField() {
-        int i,j;
-        for (i=1; i<=Depth; i++) {
-            for (j=1; j<=Width; j++) {
-                Fold[j][i] = Fnew [j][i];
+        for (int row=1; row<=FIELD_DEPTH; row++) {
+            for (int col=1; col<=FIELD_WIDTH; col++) {
+                oldField[col][row] = newField [col][row];
             }
         }
-        for (i=1; i<=Depth; i++) {
-            for (j=1; j<=Width; j++) {
-                if (Fnew[j][i]>0) {
-                    CheckNeighbours(j,i-1,j,i+1,i,j);
-                    CheckNeighbours(j-1,i,j+1,i,i,j);
-                    CheckNeighbours(j-1,i-1,j+1,i+1,i,j);
-                    CheckNeighbours(j+1,i-1,j-1,i+1,i,j);
+        for (int col=1; col<=FIELD_DEPTH; col++) {
+            for (int row=1; row<=FIELD_WIDTH; row++) {
+                if (newField[row][col]>0) {
+                    CheckNeighbours(row,col-1,row,col+1,col,row);
+                    CheckNeighbours(row-1,col,row+1,col,col,row);
+                    CheckNeighbours(row-1,col-1,row+1,col+1,col,row);
+                    CheckNeighbours(row+1,col-1,row-1,col+1,col,row);
                 }
             }
         }
